e9936a6ed29d8f072e58792bfcffe26a00cc32c1
Shash <shashgk23@gmail.com>
2023-06-21 09:55:32 -0400

    added basic frontend, error with routing

---
+++ src/Post.js
@@ -0,0 +1,13 @@
+export default function Post() {
+    return (
+        <div className="entry">
+        <div className="image"></div>
+        <div className="info">
+          <a className="author">
+            <time></time>
+          </a>
+        </div>
+
+      </div>
+    );
+}
