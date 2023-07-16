e4d2974ba29edc8e69fd9b2bfb75b37c5d5fe348
Shash <shashgk23@gmail.com>
2023-07-16 10:59:07 -0400

    the error is in post.js

--- src/pages/create_post.js
+++ src/pages/create_post.js
@@ -32,18 +32,11 @@ export default function CreatePost() {
   }
   return (
     <form onSubmit={createNewPost}>
-      <input type="title"
-             placeholder={'Title'}
-             value={title}
-             onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-             placeholder={'Summary'}
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-             onChange={ev => setFiles(ev.target.files)} />
-      <Editor value={content} onChange={setContent} />
-      <button style={{marginTop:'5px'}}>Create post</button>
+      <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary" placeholder={'Summary'} value={summary} onChange={ev => setSummary(ev.target.value)} />
+      <input type="file" onChange={ev => setFiles(ev.target.files)} />
+      <Editor value= {content} onChange={setContent} />
+      <button style= {{marginTop:'5px'}}>Create post</button>
     </form>
   );
 }
